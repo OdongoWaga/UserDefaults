61ae0ac0ce4670206759de6f4a05883a2635b2f5
Waga <wagaodongo@gmail.com>
2020-01-26 13:59:55 +0300

    defaults 512

--- UserDefaults/ContentView.swift
+++ UserDefaults/ContentView.swift
@@ -9,8 +9,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    //@State private var tapCount = 0
+    @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
+    
+   
+    
     var body: some View {
-        Text("Hello, World!")
+        Button("Tap Count: \(tapCount)") {
+            self.tapCount += 1
+            
+         UserDefaults.standard.set(self.tapCount, forKey: "Tap")
+        }
+        
+        
     }
 }
 
